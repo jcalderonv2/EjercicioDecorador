365d861f1b069c519fb0b13af875303600ccb0b5
josue <jcalderonv2@ucenfotec.ac.cr>
2018-07-25 20:59:07 -0600

    Se sube el ejercicio decorador

---
+++ EjercicioDecorador/src/objetos/Armadura.java
@@ -0,0 +1,41 @@
+package objetos;
+
+import decorador.DecoradorPersonaje;
+import decorador.Elemento;
+
+public class Armadura extends DecoradorPersonaje {
+
+	Elemento personaje;
+
+	public Armadura(Elemento pPersonaje) {
+
+		this.personaje = pPersonaje;
+
+	}
+
+	@Override
+	public String getNombre() {
+		return this.personaje.getNombre() + " con Armadura";
+	}
+
+	@Override
+	public int vida() {
+		return 20 + this.personaje.vida();
+	}
+
+	@Override
+	public int ataque() {
+		return this.personaje.ataque();
+	}
+
+	@Override
+	public int defensa() {
+		return 20 + this.personaje.defensa();
+	}
+
+	@Override
+	public int magia() {
+		return this.personaje.magia();
+	}
+
+}
---
+++ EjercicioDecorador/src/objetos/Escudo.java
@@ -0,0 +1,41 @@
+package objetos;
+
+import decorador.DecoradorPersonaje;
+import decorador.Elemento;
+
+public class Escudo extends DecoradorPersonaje {
+
+	Elemento personaje;
+
+	public Escudo(Elemento pPersonaje) {
+
+		this.personaje = pPersonaje;
+
+	}
+
+	@Override
+	public String getNombre() {
+		return this.personaje.getNombre() + " con Escudo";
+	}
+
+	@Override
+	public int vida() {
+		return 10 + this.personaje.vida();
+	}
+
+	@Override
+	public int ataque() {
+		return this.personaje.ataque();
+	}
+
+	@Override
+	public int defensa() {
+		return 40 + this.personaje.defensa();
+	}
+
+	@Override
+	public int magia() {
+		return this.personaje.magia();
+	}
+
+}
